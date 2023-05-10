68d25c2faf047d90e200ea6085d21c14e89de512
CyberneticPanda <dopeuppanda@outlook.com>
2023-05-10 00:07:42 -0700

    Fixings

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,4 @@
 hello <- function() {
   print("Hello, world!")
 }
+[Cooperative communication with humans evolved to emerge early in domestic dogs](https://datadryad.org/stash/dataset/doi:10.5061%2Fdryad.2547d7wqm)
