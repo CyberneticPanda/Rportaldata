0fb8f883729e13675b73092d0a3470f706e14e4f
CyberneticPanda <dopeuppanda@outlook.com>
2023-05-10 00:11:07 -0700

    Fixes

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,18 @@
-# References
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
 
-* [Cooperative communication with humans evolved to emerge early in domestic dogs](https://datadryad.org/stash/dataset/doi:10.5061%2Fdryad.2547d7wqm)
-*
-*
-*
+hello <- function() {
+  print("Hello, world!")
+}
